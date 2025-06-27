4de19f8f64a280ae0153e5ce60e3f0e225988b6e
markwbennett <mb@ivi3.com>
2025-06-27 17:10:30 -0500

    Update to v1.1.3 with correct SHA256 hash for data folder preferences

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -1,8 +1,8 @@
 cask "iacls-time-tracker" do
-  version "1.1.1"
-  sha256 "a30a15dbed83145f291db6b45e48902f3a2ba92b26e0333e2c7347a5cf545ac7"
+  version "1.1.3"
+  sha256 "bb9bd0988c071082fb02b9f3295b3eddc09868e1262ff58ba10d87cc5b7d9f91"
 
-  url "https://github.com/markwbennett/TimeTrackButton/raw/f455c8e08565b2254683ed0d4caa5806f6f6e1cd/TimeTracker_CPP.app.tar.gz"
+  url "https://github.com/markwbennett/TimeTrackButton/raw/9102b23fc264735da107ac5870f8f6eff7a8934e/TimeTracker_CPP.app.tar.gz"
   name "IACLS Time Tracker"
   desc "Time tracking application for legal and professional work"
   homepage "https://github.com/markwbennett/TimeTrackButton"
