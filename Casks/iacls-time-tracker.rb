940f075fe7f53c0d827af920f6555d3bbdd58c61
markwbennett <mb@ivi3.com>
2025-06-27 16:25:31 -0500

    Fix cask to point to working v1.1.1 with round button

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -1,15 +1,13 @@
 cask "iacls-time-tracker" do
-  version "1.2.0"
-  sha256 "17b930431b0d883ad5aca0711ba1d4a5bcf08315372c84bde221968511375498"
+  version "1.1.1"
+  sha256 "a30a15dbed83145f291db6b45e48902f3a2ba92b26e0333e2c7347a5cf545ac7"
 
-  url "https://github.com/markwbennett/TimeTrackButton/raw/main/TimeTracker_CPP_v1.2.0-fixed.app.tar.gz"
+  url "https://github.com/markwbennett/TimeTrackButton/raw/f455c8e08565b2254683ed0d4caa5806f6f6e1cd/TimeTracker_CPP.app.tar.gz"
   name "IACLS Time Tracker"
   desc "Time tracking application for legal and professional work"
   homepage "https://github.com/markwbennett/TimeTrackButton"
 
-  depends_on arch: :arm64
-
-  app "IACLS Time Tracker.app"
+  app "TimeTracker_CPP.app", target: "IACLS Time Tracker.app"
 
   postflight do
     system_command "/usr/bin/xattr",
@@ -21,4 +19,4 @@ cask "iacls-time-tracker" do
     "~/Documents/TimeTracker",
     "~/.config/timetracker",
   ]
-end 
+end
