35404c08f6c6b242eed7e6c530c5089ce0ef76c2
markwbennett <mb@ivi3.com>
2025-06-27 15:57:43 -0500

    Fix SHA256 hash for v1.2.0 with properly bundled Qt

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -1,6 +1,6 @@
 cask "iacls-time-tracker" do
   version "1.2.0"
-  sha256 "b23bc182b0fba4b97c6e171f67e89232141c19452165dac58f55c6a32fe7ebbc"
+  sha256 "17b930431b0d883ad5aca0711ba1d4a5bcf08315372c84bde221968511375498"
 
   url "https://github.com/markwbennett/TimeTrackButton/raw/main/TimeTracker_CPP_v1.2.0.app.tar.gz"
   name "IACLS Time Tracker"
