fdd9d82659de26c48388774a64de157ba18989e6
markwbennett <mb@ivi3.com>
2025-06-30 11:39:29 -0500

    Update to v1.3.0 with latest C++ build

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -1,16 +1,16 @@
 cask "iacls-time-tracker" do
-  version "1.1.7"
-  sha256 "34d8721aec5a8691efd02db1eb70e96174e338feb72ec193344bcfae17f66d39"
+  version "1.3.0"
+  sha256 :no_check
 
-  url "https://github.com/markwbennett/TimeTrackButton/raw/6da3cb3/TimeTracker_CPP_With_Plugins.app.tar.gz"
+  url "https://github.com/markwbennett/TimeTrackButton/raw/main/cpp_app/TimeTracker_CPP_Latest.app.tar.gz"
   name "IACLS Time Tracker"
-  desc "Simple time tracking application with floating button interface"
+  desc "Floating button time tracker for lawyers"
   homepage "https://github.com/markwbennett/TimeTrackButton"
 
   app "TimeTracker_CPP.app", target: "IACLS Time Tracker.app"
 
   zap trash: [
     "~/Documents/TimeTracker",
-    "~/Library/Preferences/org.iacls.timetracker.plist",
+    "~/Library/Preferences/com.iacls.timetracker.plist",
   ]
-end
+end 
