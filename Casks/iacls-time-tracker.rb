6cd8785e7bf86419335fdbc4dbe2f9e5bb429191
markwbennett <mb@ivi3.com>
2025-07-01 13:51:24 -0500

    Fix new project validation - v1.3.5

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -1,6 +1,6 @@
 cask "iacls-time-tracker" do
-  version "1.3.4"
-  sha256 "ffced9a29d0f6b8e201e25f0f5a73e732148e681ef424bee4da5b8b46279179f"
+  version "1.3.5"
+  sha256 "4d6d87be4a99e792ccaf3a6372180bc164a1f6274f7407ff698abc21f185b561"
 
   url "https://github.com/markwbennett/TimeTrackButton/raw/main/releases/TimeTracker_CPP_Latest.app.tar.gz"
   name "IACLS Time Tracker"
